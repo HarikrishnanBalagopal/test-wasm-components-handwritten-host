f159e231abc755392d1f8c66efe031914acecd3c
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2024-01-31 19:05:24 +0530

    feat: working version
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use wasmtime::component::*;
+use wasmtime::{Config, Engine, Store};
+
+bindgen!();
+
+fn main() -> wasmtime::Result<()> {
+    let mut config = Config::new();
+    config.wasm_component_model(true);
+    let engine = Engine::new(&config)?;
+    let component = Component::from_file(&engine, "../../manual/addcomp.wasm")?;
+
+    let linker = Linker::new(&engine);
+
+    let mut store = Store::new(&engine, ());
+
+    let (bindings, _) = Root::instantiate(&mut store, &component, &linker)?;
+
+    // let res = bindings.call_entry(&mut store, "example string here")?;
+    let res = bindings.call_length(store, "example string here")?;
+    println!("{res}");
+    Ok(())
 }
